f876273597d7dea168133eb029e88597b582e322
timf34 <farrelti@tcd.ie>
2023-05-23 00:01:59 +0100

    added comment on running task daily

--- create_file.ps1
+++ create_file.ps1
@@ -1,4 +1,6 @@
-$FolderPath = "C:\Your\Specific\Folder"
+# Create a scheduled task to run this daily using Task Scheduler
+
+$FolderPath = "C:\Users\[Your User Name]\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 $FileName = Get-Date -Format "MM-dd-yyyy"
 $FullPath = Join-Path -Path $FolderPath -ChildPath "$FileName.txt"
 
