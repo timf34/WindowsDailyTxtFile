55a8415ef20d7136a3fd3ada520407e4c7fbd703
timf34 <farrelti@tcd.ie>
2023-05-22 23:59:50 +0100

    added comment for opening file on startup

--- open_file.ps1
+++ open_file.ps1
@@ -6,4 +6,6 @@ if (Test-Path $FullPath) {
     Start-Process Notepad.exe -ArgumentList $FullPath
 }
 
-# Place this at: C:\Users\[Your User Name]\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+# Place this file at: C:\Users\[Your User Name]\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup 
+# to run it on startup
+
